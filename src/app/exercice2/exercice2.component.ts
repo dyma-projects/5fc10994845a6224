466b1a7643c4e05644daadf9ddb9b9c591a666b0
doydoy44 <laurentpapillon@free.fr>
2020-12-07 14:37:21 +0100

    Exercice2

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice2Component implements OnInit {
   public valeur: string;
+  @ViewChild('exo2', {static: true}) public el: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  showValue(): void {
+    this.valeur = this.el.nativeElement.value;
+  }
 }
